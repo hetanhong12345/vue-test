057c2e237750b834810f135ddafb4c820bcb54f5
何坦宏 <hetanhong@mofanghr.com>
2018-05-16 14:06:36 +0800

    webpack config

--- container/Kid.vue
+++ container/Kid.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="kid" flex="dir:top">
         <fish-header flex-box="0" index="3"></fish-header>
-        <div flex-box="1">kid</div>
+        <div flex-box="1">kid  is  kid </div>
     </div>
 </template>
 
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ files.map(file => {
         chunks: ['vue', entryJS],
         chunksSortMode: 'manual',
         inject: 'body',
-        filename: path.resolve(__dirname, `./html/${entryJS}.html`),
+        filename: path.resolve(__dirname, `../html/${entryJS}.html`),
         template: './index.ejs',
         minify: {//压缩HTML文件
             removeComments: true,    //移除HTML中的注释
@@ -35,7 +35,7 @@ const config = {
         path: path.resolve('../land'),
         filename: '[name].js',
         chunkFilename: '[name].[id].chunk.js',
-        publicPath: '../land/'
+        publicPath: '/land/'
     },
     resolve: {
         extensions: ['.js', '.vue', '.json'],
