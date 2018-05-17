d65486977e420475f091db0a928b9d805090bca2
何坦宏 <hetanhong@mofanghr.com>
2018-05-17 13:07:28 +0800

    base

---
+++ containers/Base.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="base"></div>
+
+</template>
+
+<script>
+    export default {
+        name: "base",
+        data() {
+        },
+        props: [],
+        components: {},
+        computed: {},
+        created() {
+        },
+        methods: {},
+        mounted() {
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
